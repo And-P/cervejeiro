172e7db9b2895f87cd4f23fc37eb176e897196d3
Andre Paiva <andrexpaiva@gmail.com>
2019-04-09 17:24:42 -0300

    c8 - v19.4 - Authenticando Login e Senha no Banco de Dados

--- src/main/java/com/umbrella/cervejeiro/config/SecurityConfig.java
+++ src/main/java/com/umbrella/cervejeiro/config/SecurityConfig.java
@@ -1,31 +1,63 @@
 package com.umbrella.cervejeiro.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.umbrella.cervejeiro.security.AppUserDetailsService;
+
 @EnableWebSecurity
+@ComponentScan(basePackageClasses= AppUserDetailsService.class)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+	@Autowired
+	UserDetailsService userDetailsService;
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 			
-		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("CADASTRO_CLIENTE");
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(passwordEncoder());
+		
+		//Authenticação feita em memória para facilitar configurações iniciais do security
+		/*auth.inMemoryAuthentication()
+			.withUser("admin")
+			.password("admin")
+			.roles("CADASTRO_CLIENTE");
+		*/	
 	
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("/layout/**")
+			.antMatchers("/images/**");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-			.anyRequest().authenticated()
-			.and().formLogin()
-			.and().csrf().disable();
+			.anyRequest()
+			.authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/login")
+			.permitAll()
+			.and()
+			.csrf().disable();
 	
 	}
 	
